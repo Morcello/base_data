9cb5e9f1efe4769181bf2198bfb037411aa08854
Timur Pavlov <brillertv@gmail.com>
2023-02-04 13:41:38 +0100

    controllers_view

--- app/controllers/register_of_owners_controller.rb
+++ app/controllers/register_of_owners_controller.rb
@@ -1,5 +1,6 @@
 class RegisterOfOwnersController < ApplicationController
   def index
+    @register_of_owners = RegisterOfOwner.all
   end
 
   def new
@@ -7,22 +8,31 @@ class RegisterOfOwnersController < ApplicationController
   end
 
   def create
-
+    @register_of_owner = RegisterOfOwner.new(register_of_owner_params)
+    if @register_of_owner.save
+      redirect_to @register_of_owners
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @register_of_owner = RegisterOfOwner.find(params[:id])
   end
 
   def update
+    @register_of_owner = RegisterOfOwner.find(params[:id])
   end
 
   def destroy
+    @register_of_owner = RegisterOfOwner.find(params[:id])
   end
 
   private
 
   def register_of_owner_params
-    params.require(:register_of_owner).permit(:first_name, :last_name)
-
+    params.require(:register_of_owner).permit(:first_name, :last_name, :middle_name, :personal_account,:city, :street,
+                                              :house_no, :apartment_no, :number_owners, :phone, :email, :home_activation_date,
+                                              :subscriber_blocking_date, :serial_number, :contractor)
   end
 end
