0c5abed386aba1249f14eb1b06a29c9b67b06e5b
Timur Pavlov <brillertv@gmail.com>
2023-04-02 21:00:20 +0200

    [2.04] new style

--- app/models/access_registry.rb
+++ app/models/access_registry.rb
@@ -1,3 +1,6 @@
 class AccessRegistry < ApplicationRecord
   belongs_to :register_of_owner, optional: true
+
+  validates :login, presence: true, absence: true, length: { minimum: 3 }
+  validates :password, presence: true, absence: true, uniqueness: true
 end
