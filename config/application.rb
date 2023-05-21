2bd668af0e7155f1ed4a6c6bffcbedfe3c4e5299
Timur Pavlov <brillertv@gmail.com>
2023-05-21 17:52:31 +0200

    fix config roo

--- config/application.rb
+++ config/application.rb
@@ -23,11 +23,11 @@ end
 
 require 'roo'
 
-xlsx = Roo::Spreadsheet.open('./new_prices.xlsx')
-xlsx = Roo::Excelx.new("./new_prices.xlsx")
+# xlsx = Roo::Spreadsheet.open('./new_prices.xlsx')
+# xlsx = Roo::Excelx.new("./new_prices.xlsx")
 
 # Use the extension option if the extension is ambiguous.
-xlsx = Roo::Spreadsheet.open('./rails_temp_upload', extension: :xlsx)
+#xlsx = Roo::Spreadsheet.open('./rails_temp_upload', extension: :xlsx)
 
-xlsx.info
+#xlsx.info
 # => Returns basic info about the spreadsheet file
